97420f289cfa50fd84e22be89cb7c11beea0f967
Iflaq khurshid <112414810+Iflaqbhat@users.noreply.github.com>
2024-10-15 14:31:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function makeid(l) {
   // write your code here
+	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+
+    // Loop to generate the string
+    for (let i = 0; i < l; i++) {
+        // Generate a random index
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        // Append the character at the random index to the result
+        result += characters[randomIndex];
+    }
+
+    return result;
+
 }
 
 // Do not change the code below.
